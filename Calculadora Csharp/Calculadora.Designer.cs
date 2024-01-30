4029f35d375a6cf94537dc5a6fd6d14402030a27
Hallan Duhke <h.duhke@gmail.com>
2024-01-30 01:35:51 -0300

    PREPARAÇÃO LAYOUT CALCULADORA

---
+++ Calculadora Csharp/Calculadora.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace Calculadora_Csharp
+{
+    partial class Calculadora
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Btn_1 = new Button();
+            Btn_2 = new Button();
+            Btn_3 = new Button();
+            Btn_6 = new Button();
+            Btn_5 = new Button();
+            Btn_4 = new Button();
+            Btn_9 = new Button();
+            Btn_8 = new Button();
+            Btn_7 = new Button();
+            textBox1 = new TextBox();
+            Btn_Divisao = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            Btn_C = new Button();
+            Btn_CE = new Button();
+            Btn_Porcentagem = new Button();
+            SuspendLayout();
+            // 
+            // Btn_1
+            // 
+            Btn_1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_1.Location = new Point(137, 421);
+            Btn_1.Name = "Btn_1";
+            Btn_1.Size = new Size(53, 55);
+            Btn_1.TabIndex = 0;
+            Btn_1.Text = "1";
+            Btn_1.UseVisualStyleBackColor = true;
+            // 
+            // Btn_2
+            // 
+            Btn_2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_2.Location = new Point(220, 421);
+            Btn_2.Name = "Btn_2";
+            Btn_2.Size = new Size(53, 55);
+            Btn_2.TabIndex = 1;
+            Btn_2.Text = "2";
+            Btn_2.UseVisualStyleBackColor = true;
+            // 
+            // Btn_3
+            // 
+            Btn_3.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_3.Location = new Point(305, 421);
+            Btn_3.Name = "Btn_3";
+            Btn_3.Size = new Size(53, 55);
+            Btn_3.TabIndex = 2;
+            Btn_3.Text = "3";
+            Btn_3.UseVisualStyleBackColor = true;
+            Btn_3.Click += button3_Click;
+            // 
+            // Btn_6
+            // 
+            Btn_6.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_6.Location = new Point(305, 350);
+            Btn_6.Name = "Btn_6";
+            Btn_6.Size = new Size(53, 55);
+            Btn_6.TabIndex = 5;
+            Btn_6.Text = "6";
+            Btn_6.UseVisualStyleBackColor = true;
+            // 
+            // Btn_5
+            // 
+            Btn_5.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_5.Location = new Point(220, 350);
+            Btn_5.Name = "Btn_5";
+            Btn_5.Size = new Size(53, 55);
+            Btn_5.TabIndex = 4;
+            Btn_5.Text = "5";
+            Btn_5.UseVisualStyleBackColor = true;
+            // 
+            // Btn_4
+            // 
+            Btn_4.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_4.Location = new Point(137, 350);
+            Btn_4.Name = "Btn_4";
+            Btn_4.Size = new Size(53, 55);
+            Btn_4.TabIndex = 3;
+            Btn_4.Text = "4";
+            Btn_4.UseVisualStyleBackColor = true;
+            // 
+            // Btn_9
+            // 
+            Btn_9.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_9.Location = new Point(305, 280);
+            Btn_9.Name = "Btn_9";
+            Btn_9.Size = new Size(53, 55);
+            Btn_9.TabIndex = 8;
+            Btn_9.Text = "9";
+            Btn_9.UseVisualStyleBackColor = true;
+            // 
+            // Btn_8
+            // 
+            Btn_8.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_8.Location = new Point(220, 280);
+            Btn_8.Name = "Btn_8";
+            Btn_8.Size = new Size(53, 55);
+            Btn_8.TabIndex = 7;
+            Btn_8.Text = "8";
+            Btn_8.UseVisualStyleBackColor = true;
+            // 
+            // Btn_7
+            // 
+            Btn_7.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_7.Location = new Point(137, 280);
+            Btn_7.Name = "Btn_7";
+            Btn_7.Size = new Size(53, 55);
+            Btn_7.TabIndex = 6;
+            Btn_7.Text = "7";
+            Btn_7.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(137, 153);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(316, 23);
+            textBox1.TabIndex = 9;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // Btn_Divisao
+            // 
+            Btn_Divisao.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_Divisao.Location = new Point(400, 280);
+            Btn_Divisao.Name = "Btn_Divisao";
+            Btn_Divisao.Size = new Size(53, 55);
+            Btn_Divisao.TabIndex = 10;
+            Btn_Divisao.Text = "/";
+            Btn_Divisao.UseVisualStyleBackColor = true;
+            Btn_Divisao.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(400, 421);
+            button2.Name = "button2";
+            button2.Size = new Size(53, 55);
+            button2.TabIndex = 11;
+            button2.Text = "+";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(137, 494);
+            button3.Name = "button3";
+            button3.Size = new Size(53, 55);
+            button3.TabIndex = 12;
+            button3.Text = "9";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(220, 494);
+            button4.Name = "button4";
+            button4.Size = new Size(53, 55);
+            button4.TabIndex = 13;
+            button4.Text = "9";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.Location = new Point(305, 494);
+            button5.Name = "button5";
+            button5.Size = new Size(53, 55);
+            button5.TabIndex = 14;
+            button5.Text = "9";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button6.Location = new Point(400, 350);
+            button6.Name = "button6";
+            button6.Size = new Size(53, 55);
+            button6.TabIndex = 15;
+            button6.Text = "x";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button7.Location = new Point(400, 494);
+            button7.Name = "button7";
+            button7.Size = new Size(53, 55);
+            button7.TabIndex = 16;
+            button7.Text = "=";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button8.Location = new Point(400, 192);
+            button8.Name = "button8";
+            button8.Size = new Size(53, 55);
+            button8.TabIndex = 17;
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // Btn_C
+            // 
+            Btn_C.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_C.Location = new Point(305, 192);
+            Btn_C.Name = "Btn_C";
+            Btn_C.Size = new Size(53, 55);
+            Btn_C.TabIndex = 18;
+            Btn_C.Text = "C";
+            Btn_C.UseVisualStyleBackColor = true;
+            // 
+            // Btn_CE
+            // 
+            Btn_CE.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_CE.Location = new Point(220, 192);
+            Btn_CE.Name = "Btn_CE";
+            Btn_CE.Size = new Size(53, 55);
+            Btn_CE.TabIndex = 19;
+            Btn_CE.Text = "CE";
+            Btn_CE.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Porcentagem
+            // 
+            Btn_Porcentagem.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_Porcentagem.Location = new Point(137, 192);
+            Btn_Porcentagem.Name = "Btn_Porcentagem";
+            Btn_Porcentagem.Size = new Size(53, 55);
+            Btn_Porcentagem.TabIndex = 20;
+            Btn_Porcentagem.Text = "%";
+            Btn_Porcentagem.UseVisualStyleBackColor = true;
+            // 
+            // Calculadora
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(846, 600);
+            Controls.Add(Btn_Porcentagem);
+            Controls.Add(Btn_CE);
+            Controls.Add(Btn_C);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(Btn_Divisao);
+            Controls.Add(textBox1);
+            Controls.Add(Btn_9);
+            Controls.Add(Btn_8);
+            Controls.Add(Btn_7);
+            Controls.Add(Btn_6);
+            Controls.Add(Btn_5);
+            Controls.Add(Btn_4);
+            Controls.Add(Btn_3);
+            Controls.Add(Btn_2);
+            Controls.Add(Btn_1);
+            Name = "Calculadora";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button Btn_1;
+        private Button Btn_2;
+        private Button Btn_3;
+        private Button Btn_6;
+        private Button Btn_5;
+        private Button Btn_4;
+        private Button Btn_9;
+        private Button Btn_8;
+        private Button Btn_7;
+        private TextBox textBox1;
+        private Button Btn_Divisao;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button Btn_C;
+        private Button Btn_CE;
+        private Button Btn_Porcentagem;
+    }
+}
---
+++ Calculadora Csharp/Calculadora.cs
@@ -0,0 +1,30 @@
+namespace Calculadora_Csharp
+{
+    public partial class Calculadora : Form
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
